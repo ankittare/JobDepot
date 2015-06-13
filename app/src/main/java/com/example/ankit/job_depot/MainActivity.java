af1f50f9359191f1ec5221598130347106c5523a
abhartha <a.bharthan@gmail.com>
2015-06-13 17:04:29 -0400

    Merge branch 'master' of /Users/anjali/Documents/JobDepot with conflicts.

--- app/src/main/java/com/example/ankit/job_depot/MainActivity.java
+++ app/src/main/java/com/example/ankit/job_depot/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.ankit.job_depot;
+package com.example.ankit.ecoordinator;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,26 +10,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.parse.Parse;
-import com.parse.ParseInstallation;
-import com.parse.ParseObject;
-
 
 public class MainActivity extends ActionBarActivity {
-    public static final String EXTRA_MESSAGE="com.example.ankit.job_depot.MESSAGE";
+
+    public static final String EXTRA_MESSAGE="com.example.ankit.ecoordinator.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "ftqZNLU8FZ8PPApaRGSZbW99xYERIqw0cWaNsKuh", "LQxbAOhhPdFDjiG3Gb1lQolW6fEgXCO94zadYO27");
-
-        /*
-        Parse API callback for saving object
-         */
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
-
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -57,19 +45,9 @@ public class MainActivity extends ActionBarActivity {
     }
     public void sendMessage(View view){
         Intent intent=new Intent(this, DisplayMessage.class);
-        EditText username=(EditText)findViewById(R.id.username);
-        //Pass password=findViewById(R.id.p)
-        String message=null;
-
-        if(username.equals("ankit")){
-            message="Welcome, "+username.getText().toString()+"!";
-            intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);
-        }
-        else{
-            TextView textView=(TextView)findViewById(R.id.error_text);
-            message=username.getText().toString();
-            textView.setText(message);
-        }
+        EditText editText=(EditText)findViewById(R.id.edit_message);
+        String message=editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
